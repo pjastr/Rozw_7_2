be14dc4384214e9b2947cab8aab80ada604866fe
Piotrek <@>
2017-11-16 17:38:17 +0100

    init

---
+++ Rozw_7_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozw_7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ccc obiekt = new Ccc();
+            obiekt.Metoda();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Aaa
+    {
+        public virtual void Metoda()
+        {
+            Console.WriteLine("Aaa");
+        }
+    }
+
+    class Bbb:Aaa
+    {
+        public override sealed void Metoda()
+        {
+            Console.WriteLine("Bbb");
+        }
+    }
+
+    class Ccc : Bbb
+    {
+        //public override void Metoda()
+        //{
+        //    Console.WriteLine("Ccc");
+        //}
+    }
+}
